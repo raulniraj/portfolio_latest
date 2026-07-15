16d6c9b1f3ad48b3db6750dbaa6760cbdf10a9d2
Niraj Raul <96654267+raulniraj@users.noreply.github.com>
2026-07-15 11:47:21 +0530

    Update index.jsx

--- app/components/homepage/about/index.jsx
+++ app/components/homepage/about/index.jsx
@@ -1,7 +1,7 @@
 // @flow strict
 
 import { personalData } from "@/utils/data/personal-data";
-import Image from "next/image";
+import Image from "public/image/profile.png";
 
 
 function AboutSection() {
